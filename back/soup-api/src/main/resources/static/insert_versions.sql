a83003591a903ba93016ea004c5a1c244ef115c9
duckbill413 <uhyeon7399@daum.net>
2024-05-21 04:26:56 +0900

    feat [BE] 필수 값 처리
    - ApiDoc 의 Path, Query Variable의 필수 값이 없으면 처리하지 않는다.

--- back/soup-api/src/main/resources/static/insert_versions.sql
+++ back/soup-api/src/main/resources/static/insert_versions.sql
@@ -1,7 +1,3 @@
 INSERT INTO springboot_version (version_name)
-VALUES ('3.3.0-SNAPSHOT'),
-       ('3.3.0-RC1'),
-       ('3.2.6-SNAPSHOT'),
-       ('3.2.5'),
-       ('3.1.12-SNAPSHOT'),
+VALUES ('3.2.5'),
        ('3.1.11');
