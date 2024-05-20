915b8d794fef78d349863a5bc97bf395ffcf469d
duckbill413 <uhyeon7399@daum.net>
2024-05-21 04:26:05 +0900

    feat [BE] 시연용 AI 기획서 생성

--- back/soup-api/src/main/java/io/ssafy/soupapi/domain/project/usecase/api/ProjectUsecaseController.java
+++ back/soup-api/src/main/java/io/ssafy/soupapi/domain/project/usecase/api/ProjectUsecaseController.java
@@ -1,8 +1,8 @@
 package io.ssafy.soupapi.domain.project.usecase.api;
 
-import io.ssafy.soupapi.domain.project.usecase.dto.request.UpdateProjectImage;
 import io.ssafy.soupapi.domain.project.usecase.application.ProjectUsecase;
 import io.ssafy.soupapi.domain.project.usecase.dto.CreateAiProposal;
+import io.ssafy.soupapi.domain.project.usecase.dto.request.UpdateProjectImage;
 import io.ssafy.soupapi.global.common.code.SuccessCode;
 import io.ssafy.soupapi.global.common.response.BaseResponse;
 import io.ssafy.soupapi.global.security.user.UserSecurityDTO;
@@ -14,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Log4j2
 @RestController
@@ -48,11 +50,21 @@ public class ProjectUsecaseController {
             @AuthenticationPrincipal UserSecurityDTO userSecurityDTO,
             @RequestBody CreateAiProposal createAiProposal
     ) {
-        CreateAiProposal response = projectUsecase.createAiProposal(createAiProposal);
+//        CreateAiProposal response = projectUsecase.createAiProposal(createAiProposal);
+//
+//        return BaseResponse.success(
+//                SuccessCode.SELECT_SUCCESS,
+//                response
+//        );
 
         return BaseResponse.success(
                 SuccessCode.SELECT_SUCCESS,
-                response
+                CreateAiProposal.builder()
+                        .background(List.of("반복되는 프로젝트 초기 설계 작업을 자동화하여 개발 생산성을 높일 수 있습니다."))
+                        .intro(List.of("웹 기반 플랫폼에서 실시간으로 팀원들과 협업하며 프로젝트 코드를 동시에 편집할 수 있습니다."))
+                        .target(List.of("동시 편집, 음성 채팅, ERD, API 설계등 개발에 필요한 다양한 기능을 하나의 플랫폼에서 제공합니다."))
+                        .result(List.of("Spring 프레임워크를 처음 접하는 개발자들도 쉽게 프로젝트를 시작할 수 있도록 도와줍니다."))
+                        .build()
         );
     }
 
